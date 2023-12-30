b845d23a1db9e3df4c2eb94e796e1a6959d0fa43
jrose0116 <jrose0116@gmail.com>
2023-12-30 16:50:06 -0500

    Create content editor demo page

---
+++ pages/docs/content-editor.js
@@ -0,0 +1,10 @@
+import Main from "@/components/main/Main"
+import ContentEditor from "@/components/contentEditor/ContentEditor"
+
+const MarkdownTest = () => {
+    return <Main>
+        <ContentEditor></ContentEditor>
+    </Main>
+}
+
+export default MarkdownTest
