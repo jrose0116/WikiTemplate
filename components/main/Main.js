624f7eab948889b19fec3ddf01acb9b9d231ebe9
jrose0116 <jrose0116@gmail.com>
2023-12-30 16:49:44 -0500

    Create main layout (nav + page holder)

---
+++ components/main/Main.js
@@ -0,0 +1,25 @@
+import globalStyles from "../../src/styles/global.module.css"
+
+const Main = ({children}) => {
+    
+    const navLinkStyle = {color: "#EEEEEE", textDecoration: "none", padding: "0px 10px"}
+
+    return (
+        <>
+        <nav style={{display: "flex", gap: "35px", width: "90%", justifyContent: "center", margin: "10px auto", backgroundColor: "#222831", fontFamily: "sans-serif", fontSize: "20px"}}>
+            <a href="/" style={navLinkStyle}><p>Home</p></a>
+            {/* <a href="/pages" style={navLinkStyle}><p>Pages</p></a> */}
+            <div className={globalStyles.dropdown}>
+                <p className={globalStyles.dropdownButton}>Docs <span className={globalStyles.dropdownArrow}></span></p>
+                <div className={globalStyles.dropdownLinks}>
+                    <a href="/docs/markdown">Markdown Styling</a>
+                    <a href="/docs/content-editor">Content Editor</a>
+                </div>
+            </div>
+        </nav>
+        <main>{children}</main>
+        </>
+    )
+}
+
+export default Main
