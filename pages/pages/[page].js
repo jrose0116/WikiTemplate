a196a41673a0037cdd4feeda008f301e8146c5bf
jrose0116 <jrose0116@gmail.com>
2024-01-01 02:57:02 -0500

    Add editing to page (auth not implemented)

--- pages/pages/[page].js
+++ pages/pages/[page].js
@@ -3,11 +3,27 @@ import Markdown from 'react-markdown'
 import style from "@/src/styles/markdownstyling.module.css"
 import { getPage } from "@/src/styles/data/pages"
 import rehypeRaw from "rehype-raw";
+import { useState } from "react";
+import ContentEditor from "@/components/contentEditor/ContentEditor"
 
-const DynamicPage = ({data}) => {
+const DynamicPage = ({header, content, title}) => {
+    const [editing, setEditing] = useState(false)
+
+    if(editing && true) // Replace true with auth later on
+        return <Main>
+        <div style={{width: "90%", margin: "10px auto", display: "flex", justifyContent: "space-around", gap: "10px"}}>
+        <div style={{backgroundColor: "#393E46", width: "100%", margin: "0", padding: "10px"}}><ContentEditor header={header} data={content} title={title} cancel={()=>setEditing(false)}></ContentEditor></div>
+        {/* <div style={{backgroundColor: "#393E46", width: "30%", margin: "0"}}>Popular Pages</div> */}
+        </div>
+    </Main>
+
     return <Main>
             <div style={{width: "90%", margin: "10px auto", display: "flex", justifyContent: "space-around", gap: "10px"}}>
-            <div style={{backgroundColor: "#393E46", width: "100%", margin: "0", padding: "10px"}}><Markdown rehypePlugins={[rehypeRaw]} className={style.markdownStyle}>{data}</Markdown></div>
+                <div style={{backgroundColor: "#393E46", width: "calc(100%)", padding: "10px", position: "relative"}}>
+                    <button style={{position: "absolute", top: "20px", right: "20px", backgroundColor: "#536878", color: "white", padding: "10px 20px", border: "none", cursor: "pointer"}} onClick={()=>setEditing(true)}>Edit</button>
+                    <div style={{backgroundColor: "#393E46", width: "100%", margin: "0"}}><Markdown rehypePlugins={[rehypeRaw]} className={style.markdownStyle}>{header}</Markdown></div>
+                    <div style={{backgroundColor: "#393E46", width: "100%", margin: "0"}}><Markdown rehypePlugins={[rehypeRaw]} className={style.markdownStyle}>{content}</Markdown></div>
+                </div>
             {/* <div style={{backgroundColor: "#393E46", width: "30%", margin: "0"}}>Popular Pages</div> */}
             </div>
         </Main>
@@ -20,11 +36,9 @@ export const getServerSideProps = async (context) => {
     if (res) {
         return {
             props: {
-                data: `
-# ${res.title}
-##### ${res.author} ${res.date ? "-" + res.date : ""}
-${res.content}
-                `,
+                header: `# ${res.title}\n##### Written by ${res.author} ${res.date ? "on " + res.date : ""}`,
+                content: res.content,
+                title: res.title
             },
         };
     }
