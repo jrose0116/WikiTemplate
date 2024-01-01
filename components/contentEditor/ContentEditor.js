1e470994c2e83cac4c6f1cd62a890503ae334493
jrose0116 <jrose0116@gmail.com>
2024-01-01 02:56:24 -0500

    Edit Content Editor for Dynamic Page Usage
    First commit of the year :)

--- components/contentEditor/ContentEditor.js
+++ components/contentEditor/ContentEditor.js
@@ -3,9 +3,20 @@ import style from "../../src/styles/markdownstyling.module.css"
 import { useState } from "react"
 import rehypeRaw from "rehype-raw";
 
-const ContentEditor = () => {
+const ContentEditor = ({data, header, title, cancel}) => {
     const [preview, setPreview] = useState(0)
-    const [content, setContent] = useState("# Content")
+    const [content, setContent] = useState(data || "# Content")
+
+    const savePage = async () => {
+        let res = await fetch('/api/editContent', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title, content })
+        })
+        window.location.reload()
+    }
 
     const buttonStyle = {backgroundColor: "#536878", color: "white", padding: "10px 20px", border: "none"}
 
@@ -16,7 +27,11 @@ const ContentEditor = () => {
                 <button style={{...buttonStyle, backgroundColor: "#36454f", fontWeight: "600"}}>Preview</button>
                 <button style={buttonStyle} onClick={()=>setPreview(2)}>Side-by-Side</button>
             </div>
-            <div style={{padding:"0 10px", width: "calc(100% - 40px)", minHeight: "300px"}}><Markdown rehypePlugins={[rehypeRaw]} className={style.markdownStyle}>{content}</Markdown></div>
+            <div style={{padding:"0 10px", width: "calc(100% - 40px)", minHeight: "300px"}}>
+                <Markdown rehypePlugins={[rehypeRaw]} className={style.markdownStyle}>{header ? header + "\n<br />\n\n" + content : content}</Markdown>
+            </div>
+            {title ? <button onClick={()=>savePage()} style={{...buttonStyle, marginLeft: "10px"}}>Save</button> : <></>}
+            {cancel ? <button onClick={()=>cancel()} style={{...buttonStyle, marginLeft: "10px"}}>Cancel</button> : <></>}
         </div>
     </div>
 
@@ -30,6 +45,8 @@ const ContentEditor = () => {
             <div style={{display: "flex", width: "calc(100% - 40px)", margin: "10px"}}>
             <textarea style={{width: "100%", height: "300px", fontFamily: "monospace", fontSize: "18px"}} onChange={(event)=>setContent(event.target.value)} value={content}/>    
             </div>
+            {title ? <button onClick={()=>savePage()} style={{...buttonStyle, marginLeft: "10px"}}>Save</button> : <></>}
+            {cancel ? <button onClick={()=>cancel()} style={{...buttonStyle, marginLeft: "10px"}}>Cancel</button> : <></>}
         </div>
     </div>
 
@@ -45,8 +62,10 @@ const ContentEditor = () => {
         <textarea style={{width: "100%", height: "300px", fontFamily: "monospace", fontSize: "18px"}} onChange={(event)=>setContent(event.target.value)} value={content} />
         </div>
         <div style={{width: "calc(50%)"}}>
-            <Markdown rehypePlugins={[rehypeRaw]} className={style.markdownStyle}>{content}</Markdown></div>
+            <Markdown rehypePlugins={[rehypeRaw]} className={style.markdownStyle}>{header ? header + "\n<br />\n\n" + content : content}</Markdown></div>
         </div>
+        {title ? <button onClick={()=>savePage()} style={{...buttonStyle, marginLeft: "10px"}}>Save</button> : <></>}
+        {cancel ? <button onClick={()=>cancel()} style={{...buttonStyle, marginLeft: "10px"}}>Cancel</button> : <></>}
     </div>
 </div>
 }
