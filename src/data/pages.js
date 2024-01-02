a4f7b77d595c51e3107c4f1aa7c46f23022bab49
jrose0116 <jrose0116@gmail.com>
2024-01-01 19:15:09 -0500

    move data/pages.js

--- src/data/pages.js
+++ src/data/pages.js
@@ -1,23 +1,21 @@
 import {pages} from "@/config/mongoCollections.js"
 
-const getAllPages = async () => {
+export const getAllPages = async () => {
     const pageCollection = await pages()
     return await (pageCollection.find({}).toArray())
 }
 
-const createPage = async (title, content, author) => {
+export const createPage = async (title, content, author) => {
     const pageCollection = await pages()
     return (await pageCollection.insertOne({title, content, author}))
 }
 
-const editContent = async (title, content) => {
+export const editContent = async (title, content) => {
     const pageCollection = await pages()
     return (await pageCollection.findOneAndUpdate({title}, {$set: {content}}))
 }
 
-const getPage = async (title) => {
+export const getPage = async (title) => {
     const pageCollection = await pages()
     return (await pageCollection.find({title}).toArray())[0]
 }
-
-export { getAllPages, createPage, getPage, editContent }
