ca0df9758eaedd6664c754c77ccc103cb71b208d
jrose0116 <jrose0116@gmail.com>
2024-01-02 05:16:35 -0500

    Use authentication / authors

--- pages/api/createPage.js
+++ pages/api/createPage.js
@@ -4,13 +4,13 @@ export default async function editPage(req, res) {
   if (req.method === 'POST') {
     try {
       const { title, content, author } = req.body;
-      if((await getPage(title))._id) return res.status(400).json({message: 'Page already exists'})
+      if((await getPage(title))?._id) return res.status(400).json({message: 'Page already exists'})
       await createPage(title, content, author);
       res.status(200).json({ message: 'Success' });
     } catch (error) {
       res.status(500).json({ message: error });
     }
   } else {
-    res.status(405).json({ nessage: 'Method Not Allowed' });
+    res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
