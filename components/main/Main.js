c4e121be61906d83e789bc208298aa74e68ded56
jrose0116 <jrose0116@gmail.com>
2024-01-02 05:16:03 -0500

    Add authentication / Login & Signout Links

--- components/main/Main.js
+++ components/main/Main.js
@@ -2,18 +2,25 @@ import Head from "next/head"
 import globalStyles from "../../src/styles/global.module.css"
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
+import { signOut, useSession } from "next-auth/react";
 
 const Main = ({children}) => {
-    
-    const navLinkStyle = {color: "#EEEEEE", textDecoration: "none", padding: "0px 10px"}
+    const {data, status} = useSession()
 
+    const doSignOut = async () => {
+        await signOut()
+        window.location.href = "/"
+    }
+
+    const navLinkStyle = {color: "#EEEEEE", textDecoration: "none", padding: "0px 10px"}
+    
     return (
         <>
         <Head>
             <title>Wiki Template</title>
         </Head>
         <ToastContainer />
-        <nav style={{display: "flex", gap: "35px", width: "90%", justifyContent: "center", margin: "10px auto", backgroundColor: "#222831", fontFamily: "sans-serif", fontSize: "20px"}}>
+        <nav style={{display: "flex", gap: "35px", width: "90%", justifyContent: "center", margin: "10px auto", backgroundColor: "#222831", fontFamily: "sans-serif", fontSize: "20px", position: "relative"}}>
             <a href="/" style={navLinkStyle}><p>Home</p></a>
             <a href="/pages" style={navLinkStyle}><p>Pages</p></a>
             <div className={globalStyles.dropdown}>
@@ -23,6 +30,10 @@ const Main = ({children}) => {
                     <a href="/docs/content-editor">Content Editor</a>
                 </div>
             </div>
+            { (status !== "loading") ? (data) ?
+            <a onClick={()=>doSignOut()} style={{...navLinkStyle, position: "absolute", right: "10px"}}><p>Sign Out</p></a> : 
+            <a href="/login" style={{...navLinkStyle, position: "absolute", right: "10px"}}><p>Log In</p></a>
+            : <></>}
         </nav>
         <main>{children}</main>
         <style jsx global>{`body {
