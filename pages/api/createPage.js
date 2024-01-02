b89bd2dbe6802fa7b12f96e05615fab578e30434
jrose0116 <jrose0116@gmail.com>
2024-01-01 19:14:30 -0500

    Handle duplicate page

--- pages/api/createPage.js
+++ pages/api/createPage.js
@@ -1,15 +1,16 @@
-import { createPage } from '@/src/styles/data/pages';
+import { createPage, getPage } from '@/src/data/pages';
 
 export default async function editPage(req, res) {
   if (req.method === 'POST') {
     try {
       const { title, content, author } = req.body;
+      if((await getPage(title))._id) return res.status(400).json({message: 'Page already exists'})
       await createPage(title, content, author);
       res.status(200).json({ message: 'Success' });
     } catch (error) {
-      res.status(500).json({ error });
+      res.status(500).json({ message: error });
     }
   } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
+    res.status(405).json({ nessage: 'Method Not Allowed' });
   }
 }
