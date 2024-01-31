0c778d1d89d72113576159e98da75cfc415a8b9f
jrose0116 <jrose0116@gmail.com>
2024-01-31 00:24:50 -0500

    Update mongo settings + add .env to gitignore

--- config/settings.js
+++ config/settings.js
@@ -1,5 +1,5 @@
 export const mongoConfig = {
   // serverUrl: "mongodb://127.0.0.1:27017/",
-  serverUrl: "mongodb://127.0.0.1:27017/",
-  database: "WikiPages",
+  serverUrl: `mongodb+srv://jrose0116:${process.env.mongoPass}@cluster.nbrlskp.mongodb.net/?retryWrites=true&w=majority`,
+  database: "JacobRose_Wiki",
 };
